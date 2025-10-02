46c8ad52d7e5c4f0532a91f4b5ad548307d57501
efan-mc <efangeorge@gmail.com>
2025-10-02 21:42:50 +0100

    add: added route for team url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import TeamBuilder from "./pages/TeamBuilder";
+import TeamView from "./pages/TeamView";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<TeamBuilder />} />
+      <Route path="/t/:slug" element={<TeamView />} />
     </Routes>
   );
 }
